fe0121665d0f8617ee8a624faf0038ae35d0f1b2
Sahil Kadawla <84659499+sahilkadawla@users.noreply.github.com>
2024-09-06 16:53:58 +0530

    Update script.js
    old api key expired 
    updated new api key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apikey": WEATHER_API_KEY,
+    apikey: "aa5af640f65a964a73dcea81b9405e02",
     fetchWeather : function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
